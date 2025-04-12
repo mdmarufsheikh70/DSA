218bea528d184c4f7ebacb39cf1be9840b4d0614
Maruf <mdmarufsheikh707@gmail.com>
2025-04-12 22:50:32 +0600

    Add files via upload

---
+++ Selection.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int A[10]={1,2,3,4,5},n;
+    cout<<"Enter your index number:";
+    cin>>n;
+    cout<<"the value of index "<<n<<" is "<<A[n]<<endl;
+return 0;
+}
