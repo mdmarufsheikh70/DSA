38e4e4935bfd278d5a8f2f9d7891478e899c1bad
Maruf <mdmarufsheikh707@gmail.com>
2025-04-15 17:31:20 +0600

    Add files via upload

---
+++ Deletion3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int p=5;
+    int A[50]={10,20,30,40,50};
+    cout<<"previous Array: ";
+    for(int i=0;i<5;i++)
+    {
+        cout<<A[i]<<"\t";
+    }
+    cout<<endl;
+    for(int i=p-1;i<4;i++){
+    A[i]=A[i+1];
+    }
+    cout<<"After deletion: ";
+     for(int i=0;i<4;i++)
+    {
+        cout<<A[i]<<"\t";
+    }
+    return 0;
+}
