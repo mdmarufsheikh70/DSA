7d1e35ca9ebdcc913b7369d16957653c8ca9167e
Maruf <mdmarufsheikh707@gmail.com>
2025-04-16 14:31:57 +0600

    Update Sorting.cpp

--- Sorting.cpp
+++ Sorting.cpp
@@ -1,23 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int A[5]={50,20,30,40,10};
+    int A[5] = {50,20,30,40,10};
     cout<<"Before sorting : ";
-    for(int i=0;i<5;i++){
-    cout<<A[i]<<"\t";
+    for(int i = 0; i < 5; i++){
+    cout<<A[i] << "\t";
   }
   cout<<endl;
-  for(int i=0; i<4;i++){
-    for(int j=0;j<5;j++){
-        if(A[j]<A[j+1]){
-            int tem=A[j];
-            A[j]=A[i];
-            A[i]=tem;
+  for(int i = 0; i < 4; i++){
+    for(int j = 0; j < 5; j++){
+        if(A[j]< A [j+1]){
+            int tem = A[j];
+            A[j] = A[i];
+            A[i] = tem;
         }
     }
   }
    cout<<"After sorting : ";
-  for(int i=0;i<5;i++){
+  for(int i = 0; i < 5; i++){
   cout<<A[i]<<"\t";
   }
 return 0;
