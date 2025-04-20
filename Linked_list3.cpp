af3220d09eb3bec352ec312b5bc5f03ca3dc40b5
Maruf <mdmarufsheikh707@gmail.com>
2025-04-20 18:59:41 +0600

    Add files via upload

---
+++ Linked_list3.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+class node {
+public:
+    int data;
+    node *next;
+
+    node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insert_at_head(node *&head, int x) {
+    node *new_node = new node(x);
+    new_node->next = head;
+    head = new_node;
+}
+
+void insert_at_tail(node *&head, int x) {
+    node *new_node = new node(x);
+
+    if (head == NULL) {
+        head = new_node;
+    } else {
+        node *temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+}
+
+void insert_any_position(node *&head,int x,int position) {
+    if (position == 1) {
+        insert_at_head(head, x);
+        return;
+    }
+    node *new_node = new node(x);
+    node *temp = head;
+    int current_position =1;
+    while (temp != NULL && current_position < position-1) {
+        temp = temp->next;
+        current_position++;
+    }
+
+    if (temp == NULL) {
+        insert_at_tail(head, x);
+    } 
+    else {
+        new_node->next = temp->next;
+        temp->next = new_node;
+    }
+}
+
+void display(node *head) {
+    node *temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    node *head = NULL;
+    insert_at_head(head, 2);
+    display(head);
+    cout << endl;
+    insert_at_head(head, 1);
+    display(head);
+    cout << endl;
+    insert_at_tail(head, 4);
+    display(head);
+    cout << endl;
+    insert_at_tail(head, 5);
+    display(head);
+    cout << endl;
+    insert_any_position(head, 3, 3);
+    display(head);
+    cout << endl;
+    return 0;
+}
