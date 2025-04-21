c6b363a4381251e3c1011588bd592bad8caff29d
Maruf <mdmarufsheikh707@gmail.com>
2025-04-21 20:56:24 +0600

    Add files via upload

---
+++ Linked_list7.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+using namespace std;
+
+class node {
+public:
+    int data;
+    node *next;
+
+    node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insert_at_head(node *&head, int x) {
+    node *new_node = new node(x);
+    new_node->next = head;
+    head = new_node;
+}
+
+void insert_at_tail(node *&head, int x) {
+    node *new_node = new node(x);
+
+    if (head == NULL) {
+        head = new_node;
+    } else {
+        node *temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+}
+
+void insert_any_position(node *&head,int position,int x) {
+    if (position == 1) {
+        insert_at_head(head, x);
+        return;
+    }
+
+    node *new_node = new node(x);
+    node *temp = head;
+    int current_position = 1;
+
+    
+    while (temp != NULL && current_position < position-1) {
+        temp = temp->next;
+        current_position++;
+    }
+
+    if (temp == NULL) {
+        
+        insert_at_tail(head, x);
+    } else {
+        new_node->next = temp->next;
+        temp->next = new_node;
+    }
+}
+
+void delete_at_head(node *&head){
+    node *temp=head;
+    head=head->next;
+    delete(temp);
+}
+
+void delete_at_tail(node *&head){
+    node *second_last_node= head;
+    while(second_last_node->next->next!=NULL){
+        second_last_node=second_last_node->next;
+    }
+    node *temp=second_last_node->next;
+    second_last_node->next=NULL;
+    delete(temp);
+}
+
+void delete_at_any_position(node *&head, int position){
+    if(position==1){
+        delete_at_head(head);
+        return;
+    }
+    int currant_positin=1;
+    node *previous=head;
+    while(currant_positin!=position-1){
+        previous=previous->next;
+        currant_positin++;
+    }
+    node *temp=previous->next;
+    previous->next=previous->next->next;
+    delete(temp);
+}
+
+void display(node *head) {
+    node *temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    node *head = NULL;
+    insert_at_head(head, 2);
+    display(head);
+    cout << endl;
+    insert_at_head(head, 1);
+    display(head);
+    cout << endl;
+    insert_at_tail(head, 4);
+    display(head);
+    cout << endl;
+    insert_at_tail(head, 5);
+    display(head);
+    cout << endl;
+    insert_any_position(head, 3, 3);
+    display(head);
+    cout << endl;
+    cout<<"Delete at head :\n";
+    delete_at_head(head);
+    display(head);
+    cout << endl;
+    insert_any_position(head, 1, 1);
+    display(head);
+    cout << endl;
+    cout<<"Delete at tail :\n";
+    delete_at_tail(head);
+    display(head);
+    cout << endl;
+    insert_any_position(head, 5, 5);
+    display(head);
+    cout << endl;
+    cout<<"Delete at any position = 3 :\n";
+    delete_at_any_position(head,3);
+    display(head);
+    cout << endl;
+    insert_any_position(head, 3, 3);
+    display(head);
+    cout << endl;
+    return 0;
+}
